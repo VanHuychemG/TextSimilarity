d7f0c552222a40f5733002e0e77cda539322cd39
iframeworx <geert@iframeworx.be>
2016-12-21 10:49:47 +0100

    stringcomparer & gethashcode alt

--- TextSimilarity.Common/Extensions/String/ShinglesExtension.cs
+++ TextSimilarity.Common/Extensions/String/ShinglesExtension.cs
@@ -26,16 +26,10 @@ namespace TextSimilarity.Common.Extensions.String
 
         public static HashSet<string> WordShingles(this string value, int shingleSize = DefaultShingleSize, int shingleOverlap = DefaultShingleOverlap)
         {
-<<<<<<< HEAD
             if (shingleOverlap >= shingleSize) throw new ArgumentException("Shingle overlap cannot be bigger than the shingle size");
 
             var result = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-=======
-            if (shingleOverlap >= shingleSize) throw new ArgumentException("Shi" +
-                                                                           "ngle overlap cannot be bigger than the shingle size");
-            var result = new HashSet<string>(
-                CultureInfo.InvariantCulture.CompareInfo.GetStringComparer(CompareOptions.IgnoreCase));
->>>>>>> c87080c27f0b86ef3366c00f28747031d43eed52
+
             if (string.IsNullOrEmpty(value)) return result;
 
             var words = Regex.Split(value, @"[\s\!\?\.\,\-]+", RegexOptions.CultureInvariant);
